1e0756b21f3869db0fa191af7aa54eff9835e12a
sudhishP1234 <sudhishpandey2599@gmail.com>
2023-09-26 11:37:10 +0530

    change mass

---
+++ Js_practice/Filter.js
@@ -0,0 +1,64 @@
+// const numNo =[8,9,7,5]
+//  const n=numNo.filter((num)=>{
+//     return num >5
+// })
+// console.log(n);
+
+//===========================
+// const ages =[33,25,48,33,58,22,26,24,45];
+//  const age =ages.filter(function(age){
+//     if(age>=35){
+//    return true;
+//     }
+//  })
+//  console.log(age);
+//=========================
+
+// var companies = [
+//   { name: "google", category: "service based", start: "1981" },
+//   { name: "Amezon", category: "product based", start: "2003" },
+//   { name: "paytm", category: "product based", start: "2006" },
+// ];
+//  const ftr =companies.filter(function(company){
+//    return company.category === "service based" 
+// //   if(company.category==="service based"){
+// //     return true;
+// })
+// console.log(ftr);
+
+//===========or==============
+// const fltr =companies.filter(company=> company.category === "product based");
+// console.log(fltr);
+
+//======================
+// const arr =[5,8,9,7,2];
+// function isOdd(x){
+//     return x % 2;
+// }
+// const output =arr.filter(isOdd);
+// console.log(output);
+
+// //========================
+// const arr1 =[5,8,9,7,2];
+// function isEven(x){
+//     return x % 2===0;
+// }
+// const output1 =arr.filter(isEven);
+// console.log(output1);
+
+//===========================
+// const arr =[5,8,9,7,2];
+// function isGreaterthen(x){
+//     return x > 7;
+// }
+// const output =arr.filter(isGreaterthen);
+// console.log(output);
+
+//===========================
+// const arr1 =[5,8,9,7,2];
+// function islessThen(x){
+//     return x < 8;
+// }
+// const output1 =arr1.filter(islessThen);
+// console.log(output1);
+
---
+++ Js_practice/Map.js
@@ -0,0 +1,41 @@
+// const myNum = [4,5,6,8,9]
+// const newNum =myNum.map((num)=>{return num +2})
+
+//const newNum1=myNum.map((num)=>num * 5)
+// //.map((num)=>num+5)
+// .filter((num)=>num<9)
+// .reduce((acc,curr)=>{
+//     return acc +curr
+// },0);
+//  console.log(newNum)
+
+//===============================
+
+//   const dummy =companies.map((company,index)=>{
+//       return `${company.name} ${company.category} ${company.start}`
+//   })
+//   console.log(dummy);
+
+//================================
+
+// const arr =[5,6,7,8,4];
+// function double(x){
+//     return x*2;
+// }
+// const output =arr.map(double)
+// console.log(output);
+
+// //===============================
+// const numbers = [1, 2, 3, 4, 5];
+// const squaredNumbers = numbers.map((number) => number ** 2);
+
+// console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
+
+//=================================
+// const names = ["Alice", "Bob", "Charlie", "David"];
+// const nameLengths = names.map(function(name) {  
+//   return name.length;
+// });                                      => Output: [5, 3, 7, 5]
+// console.log(nameLengths); 
+
+  
